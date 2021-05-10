e51b72574d342b39acd59389533debb2a726fccc
TOSHIBA\Karol <karol.kawczynski@wp.pl>
2021-05-10 14:23:33 +0200

    Dodanie pierwszego DTO

---
+++ ViewModels/StylistsWorkingHoursDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairdressingStudio.ViewModels
+{
+    public class StylistsWorkingHoursDTO
+    {
+        public int Id { get; set; }
+        public int StylistId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
